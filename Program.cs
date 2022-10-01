52c5e10de6dfa88fae1ee4e662e4b6c87a4878a4
GalinaS <peraskevasokolova@gmail.com>
2022-10-01 17:17:26 +0400

    added method to create a sorted array

--- Program.cs
+++ Program.cs
@@ -17,17 +17,59 @@ string[] InputArray(int size)
 
 void PrintArray(string[] ArrayPr)
 {
-    Console.Write("Исходный массив: [");
-    for (int i = 0; i < ArrayPr.Length - 1; i++)
+    if (ArrayPr.Length > 0)
     {
-        Console.Write("\"" + ArrayPr[i] + "\"" + ", ");
+        Console.Write("[");
+        for (int i = 0; i < ArrayPr.Length - 1; i++)
+        {
+            Console.Write("\"" + ArrayPr[i] + "\"" + ", ");
+        }
+        Console.Write("\"" + ArrayPr[ArrayPr.Length - 1] + "\"" + "]");
     }
-    Console.Write("\"" + ArrayPr[ArrayPr.Length - 1] + "\"" + "]");
+    else
+    {
+        Console.Write("[]");
+    }
 }
 
-Console.Write("Введите количество элементов в массиве: ");
-int count = Convert.ToInt32(Console.ReadLine());
-string[] array = InputArray(count);
-//Console.Write("Исходный массив: ");
-PrintArray(array);
+// Создание массива из 1 - 3-х символьных элементов
 
+string[] CreationArray(string[] ArrayCr)
+{
+    int currentSize = 0;
+    string[] arrayFinal = new string[currentSize];
+    int j = 0;
+    for (int i = 0; i < ArrayCr.Length; i++)
+    {
+        if (ArrayCr[i].Length <= 3)
+        {
+            currentSize++;
+            Array.Resize(ref arrayFinal, currentSize);
+            arrayFinal[j] = ArrayCr[i];
+            j++;
+        }
+
+    }
+    return arrayFinal;
+}
+
+int count;
+do
+{
+    Console.Write("Введите количество элементов в массиве: ");
+    count = Convert.ToInt32(Console.ReadLine());
+    if (count <= 0)
+    {
+      Console.WriteLine("Необходимо ввести натуральное число");
+    } 
+}
+while (count <= 0);
+
+string[] arraySource = InputArray(count);
+Console.WriteLine("Исходный массив: ");
+PrintArray(arraySource);
+Console.WriteLine();
+Console.WriteLine();
+string[] arrayNew = CreationArray(arraySource);
+Console.WriteLine("Итоговый массив: ");
+PrintArray(arrayNew);
