67ef5b6d2afcadd141514a69b5711d768bc534c1
GalinaS <peraskevasokolova@gmail.com>
2022-10-01 13:58:18 +0400

    added array input method

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу, которая из имеющегося массива строк 
+//формирует массив из строк, длина которых меньше либо равна 3 символа
+
+//метод ввода массива строк с клавиатуры
+string[] InputArray(int size)
+{
+    string[] Array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i+1} -й элемент массива");
+        Array[i]  = Console.ReadLine();
+    }
+    return Array;
+}
