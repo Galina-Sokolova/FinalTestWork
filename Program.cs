489abeafe1fea3e5d3fcc5c70ba68027acb68754
GalinaS <peraskevasokolova@gmail.com>
2022-10-01 14:35:54 +0400

    added method to output array to console

--- Program.cs
+++ Program.cs
@@ -4,11 +4,30 @@
 //метод ввода массива строк с клавиатуры
 string[] InputArray(int size)
 {
-    string[] Array = new string[size];
+    string[] ArrayIn = new string[size];
     for (int i = 0; i < size; i++)
     {
-        Console.Write($"Введите {i+1} -й элемент массива");
-        Array[i]  = Console.ReadLine();
+        Console.Write($"Введите {i+1} -й элемент массива: ");
+        ArrayIn[i]  = Console.ReadLine();
     }
-    return Array;
+    return ArrayIn;
 }
+
+//метод вывода массива в консоль
+
+void PrintArray(string[] ArrayPr)
+{
+    Console.Write("Исходный массив: [");
+    for (int i = 0; i < ArrayPr.Length - 1; i++)
+    {
+        Console.Write("\"" + ArrayPr[i] + "\"" + ", ");
+    }
+    Console.Write("\"" + ArrayPr[ArrayPr.Length - 1] + "\"" + "]");
+}
+
+Console.Write("Введите количество элементов в массиве: ");
+int count = Convert.ToInt32(Console.ReadLine());
+string[] array = InputArray(count);
+//Console.Write("Исходный массив: ");
+PrintArray(array);
+
